4e7c28ffad7c7ffeadb39e3709bdfa50dc662167
Sikai Li <sli-ext@lequipe.fr>
2020-03-24 23:19:59 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sikaili/p5js.git master:master
+git push -f git@github.com:sikaili/p5js-host.git master:master
 
 # cd -
